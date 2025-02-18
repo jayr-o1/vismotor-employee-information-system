8ec4e3c8da5c7f571ae0e8d1b1bc1461e0854dbe
jayr-o1 <olores.jayrm@gmail.com>
2025-02-18 10:21:46 +0800

    fixed ui and changed colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import Wallet from './pages/Wallet';
-import Category from './pages/Category';
+import Employees from './pages/Employees';
 import ScanQR from './pages/ScanQR';
 import Settings from './pages/Settings';
 
@@ -13,8 +12,7 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/category" element={<Category />} />
+          <Route path="/employees" element={<Employees />} />
           <Route path="/scan-qr" element={<ScanQR />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
